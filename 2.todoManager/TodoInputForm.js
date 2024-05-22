acb358ce157898fa054a2b4c6ae7f0673ed8f094
seul-dev <seul.dev@gmail.com>
2024-05-22 22:37:03 +0900

    refactor TodoInputForm to use constants for priority options

--- 2.todoManager/TodoInputForm.js
+++ 2.todoManager/TodoInputForm.js
@@ -1,37 +1,4 @@
-// export default function TodoInputForm({ $target, onSubmit }) {
-//   this.$element = document.createElement('div');
-
-//   this.render = () => {
-//     this.$element.innerHTML = `
-//       <form>
-//         <input type="text" placeholder="할 일을 입력하세요.">
-//         <button>추가</button>
-//       </form>
-//       <button class="RemoveAllButton">전체 삭제</button>
-//     `;
-//   };
-
-//   $target.appendChild(this.$element);
-
-//   this.render();
-
-//   this.$element.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     const $input = e.target.querySelector('input');
-
-//     onSubmit($input.value);
-
-//     $input.value = '';
-//     $input.focus();
-//   });
-
-//   this.$element
-//     .querySelector('.RemoveAllButton')
-//     .addEventListener('click', () => {
-//       window.dispatchEvent(new CustomEvent(REMOVE_ALL));
-//     });
-// }
+import { PRIORITY } from './constants.js';
 
 export default function TodoInputForm({ $target, onSubmit }) {
   this.$element = document.createElement('div');
@@ -43,23 +10,17 @@ export default function TodoInputForm({ $target, onSubmit }) {
       <input type="text" id='todo-text' placeholder="할 일을 작성해주세요.">
       <button type='submit'>추가</button>
       <div class="priority">
-        <div>
-          <input type="radio" id="low" name="priority" value="low" checked>
-          <label for="low">Low</label>
-        </div>
-        <div>
-          <input type="radio" id="medium" name="priority" value="medium">
-          <label for="medium">Medium</label>
-        </div>
-        <div>
-          <input type="radio" id="high" name="priority" value="high">
-          <label for="high">High</label>
-        </div>
-        <div>
-          <input type="radio" id="very-high" name="priority" value="very-high">
-          <label for="very-high">Very High</label>
-        </div>
-      </div>
+         ${Object.entries(PRIORITY)
+           .map(
+             ([value, label], index) =>
+               `<div>
+                <input type="radio" id="${value}" name="priority" value="${value}" ${
+                 index === 0 ? 'checked' : ''
+               }>
+                <label for="${value}">${label}</label>
+              </div>`
+           )
+           .join('')}  
       </div>
     </form>
     `;
