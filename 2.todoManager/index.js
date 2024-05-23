66cd874aa653d908fef9450dd03d87e58a766eb9
seul-dev <seul.dev@gmail.com>
2024-05-23 19:58:13 +0900

    docs: add memo

--- 2.todoManager/index.js
+++ 2.todoManager/index.js
@@ -4,11 +4,11 @@ import TodoFilter from './TodoFilter.js';
 import { Filter } from './constants.js';
 
 function App({ $target, initialState }) {
-  this.state = initialState;
+  this.todos = initialState;
 
   this.setState = (nextState) => {
-    this.state = nextState;
-    todoList.setState(this.state);
+    this.todos = nextState;
+    todoList.setState(this.todos);
   };
 
   const todoInputForm = new TodoInputForm({
@@ -16,7 +16,7 @@ function App({ $target, initialState }) {
     onSubmit: ({ text, priority }) => {
       // todoInputForm에서 받아온 데이터를 state에 추가
       const nextState = [
-        ...this.state,
+        ...this.todos,
         {
           id: crypto.randomUUID(),
           text,
@@ -32,22 +32,23 @@ function App({ $target, initialState }) {
     $target,
     initialState: 'all',
     onChange: (filterName) => {
-      const newState = Filter[filterName].filterFunc(this.state);
+      const newState = Filter[filterName].filterFunc(this.todos);
       todoList.setState(newState);
     },
   });
 
   const todoList = new TodoList({
     $target,
-    initialState: this.state,
+    initialState: this.todos,
     onToggle: (id) => {
-      const nextState = this.state.map((todo) =>
+      const nextState = this.todos.map((todo) =>
         todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
       );
+      // Todo: filter 상태에 따라서 필터링된 상태로 업데이트 시키도록 수정
       this.setState(nextState);
     },
     onEdit: (id) => {
-      const nextState = this.state.map((todo) =>
+      const nextState = this.todos.map((todo) =>
         todo.id === id
           ? { ...todo, text: prompt('수정할 내용을 입력하세요', todo.text) }
           : todo
